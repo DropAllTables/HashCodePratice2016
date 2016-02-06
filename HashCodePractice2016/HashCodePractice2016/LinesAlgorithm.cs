4dcddf8ce263f6e2ff96b48e349678e9ac65cf87
JPonte <joaopcponte@gmail.com>
2016-02-06 18:26:31 +0000

    Lines Algorithm class.

---
+++ HashCodePractice2016/HashCodePractice2016/LinesAlgorithm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodePractice2016
+{
+    public static class LinesAlgorithm
+    {
+        public static List<Command> solve(PictureDescription pd)
+        {
+            PictureDescription auxPd = new PictureDescription(pd.NumRows, pd.NumCols);
+
+            return List<Command>();
+        }
+    }
+}
