41cf92432d417ffffd76e18580e31f48542b1a3d
Luis Reis <luiscubal@gmail.com>
2016-02-06 16:16:58 +0000

    Created PictureDescription class

---
+++ HashCodePractice2016/HashCodePractice2016/PictureDescription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodePractice2016
+{
+    public sealed class PictureDescription
+    {
+        private bool[,] data;
+
+        public PictureDescription(int nRows, int nCols)
+        {
+            data = new bool[nRows, nCols];
+        }
+
+        public int NumRows
+            => data.GetLength(0);
+
+        public int NumCols
+            => data.GetLength(1);
+
+        public bool IsFilled(int x, int y)
+        {
+            return data[y, x];
+        }
+    }
+}
